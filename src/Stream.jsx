c87ac43d11e0a76296f314d23c51763d0dd8c5f8
liefjamescain <liefjamescain@gmail.com>
2021-02-22 01:17:00 -0800

    stream changes

--- src/Stream.jsx
+++ src/Stream.jsx
@@ -1,3 +1,21 @@
+const VALID_CHARS = `abcdefghijklmnopqrstuvwxyz0123456789$+-*/=%"'#&_(),.;:?!\\|{}<>[]^~`;
+// const VALID_CHARS = `abcdefghijklmnopqrstuvwxyz0123456789`;
+
+const MIN_STREAM_SIZE = 15;
+const MAX_STREAM_SIZE = 50;
+
+const getRandomCharInRange = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+const getRandomChar = () => {
+  VALID_CHARS.charAt(Math.floor(Math.random() * VALID_CHARS.length));
+}
+
+const generateStream = () => {
+  new Array(getRandomCharInRange(MIN_STREAM_SIZE, MAX_STREAM_SIZE))
+    .fill()
+    .map(_ => getRandomChar());
+}
+
 const Stream = () => {
   return (
     <div style={{
@@ -10,7 +28,7 @@ const Stream = () => {
       textShadow: '0px 0px 8px rgba(102, 78, 174, 0.8)',
       fontSize: 50
     }}>
-      {'test'.split().map(char => (
+      {'cain'.split().map(char => (
         <span style={{marginTop: -12}}>{char}</span>
       ))}
     </div>
