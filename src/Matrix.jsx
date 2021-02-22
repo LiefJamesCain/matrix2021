ecf23ef741407a8d942cfddb5d33b844093821d3
liefjamescain <liefjamescain@gmail.com>
2021-02-22 00:31:12 -0800

    colors

---
+++ src/Matrix.jsx
@@ -0,0 +1,18 @@
+import Stream from './Stream'
+
+const Matrix = () => {
+  return (
+    <div style={{
+      position: 'fixed',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      background: 'black'
+    }}>
+      <Stream/>
+    </div>
+  );
+}
+
+export default Matrix;
---
+++ src/Stream.jsx
@@ -0,0 +1,15 @@
+const Stream = () => {
+  return (
+    <span style={{
+      color: '#664eae',
+      writingMode: 'vertical-rl',
+      textOrientation: 'upright',
+      whiteSpace: 'nowrap',
+      userSelect: 'none',
+      textShadow: '0px 0px 8px rgba(102, 78, 174, 0.8)',
+      fontSize: 50
+    }}>test</span>
+  )
+}
+
+export default Stream;
