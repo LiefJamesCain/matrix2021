57be3f4f580a0fc6c70f8eb6dd02f997a97ceb54
liefjamescain <liefjamescain@gmail.com>
2021-02-22 23:15:59 -0800

    file removal + random character generator attempt

--- src/Matrix.jsx
+++ src/Matrix.jsx
@@ -1,18 +1,20 @@
-import Stream from './Stream'
+import Stream from "./Stream";
 
 const Matrix = () => {
   return (
-    <div style={{
-      position: 'fixed',
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      background: 'black'
-    }}>
-      <Stream/>
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        background: "black",
+      }}
+    >
+      <Stream />
     </div>
   );
-}
+};
 
 export default Matrix;
--- src/Stream.jsx
+++ src/Stream.jsx
@@ -1,31 +1,65 @@
-let validChars = `abcdefghijklmnopqrstuvwxyz0123456789$+-*/=%"'#&_(),.;:?!\\|{}<>[]^~`;
-let minStreamSize = 15;
-let maxStreamSize = 50;
+const validChars = `abcdefghijklmnopqrstuvwxyz0123456789$+-*/=%"'#&_(),.;:?!\\|{}<>[]^~`;
+const minStreamSize = 15;
+const maxStreamSize = 50;
 
-const getRandomCharInRange = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+const getRandomCharInRange = (min, max) =>
+  Math.floor(Math.random() * (max - min)) + min;
 
-const getRandomChar = () => validChars.charAt(Math.floor(Math.random() * validChars.length));
+const getRandomChar = () =>
+  validChars.charAt(Math.floor(Math.random() * validChars.length));
 
 const generateStream = () => {
-  new Array(getRandomCharInRange(minStreamSize, maxStreamSize)).fill().map(_ => getRandomChar());
-}
+  let chars = new Array(getRandomCharInRange(minStreamSize, maxStreamSize));
+  chars.map((char) => {
+    return getRandomChar();
+  });
+};
 
 const Stream = () => {
+  const raindrop = generateStream();
+  const lief = "liefjamescain";
   return (
-    <div style={{
-      fontFamily: 'modernWakandan',
-      color: '#664eae',
-      writingMode: 'vertical-rl',
-      textOrientation: 'upright',
-      whiteSpace: 'nowrap',
-      userSelect: 'none',
-      textShadow: '0px 0px 8px rgba(102, 78, 174, 0.8)',
-      fontSize: 50
-    }}>
-      {/* {generateStream().map(char => (<span key={char.toString()}>{char}</span>))} */}
-      {'lief'.split().map(char => (<span key={char.toString()} style={{marginTop: -12}}>{char}</span>))}
+    <div
+      style={{
+        fontFamily: "modernWakandan",
+        color: "#664eae",
+        writingMode: "vertical-rl",
+        textOrientation: "upright",
+        whiteSpace: "nowrap",
+        userSelect: "none",
+        textShadow: "0px 0px 8px rgba(102, 78, 174, 0.8)",
+        fontSize: 50,
+      }}
+    >
+      {/* {raindrop.map((char, index) => (
+        <span
+          style={{
+            color: index === raindrop.length - 1 ? "#fff" : undefined,
+            opacity: index < 6 ? 0.1 + index * 0.15 : 1,
+            textShadow:
+              index === raindrop.length - 1 ? "0px 0px 20px #fff" : undefined,
+            marginTop: -12,
+          }}
+        >
+          {char}
+        </span>
+      ))} */}
+      {lief.split().map((char, index) => (
+        <span
+          key={char.toString()}
+          style={{
+            color: index === lief.length - 1 ? "#fff" : undefined,
+            // opacity: index < 6 ? 0.1 + index * 0.15 : 1,
+            textShadow:
+              index === lief.length - 1 ? "0px 0px 20px rgba(255, 255, 255, 1)" : undefined,
+            marginTop: -12,
+          }}
+        >
+          {char}
+        </span>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default Stream;
